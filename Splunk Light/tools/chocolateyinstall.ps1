799e6fb6160a6c991803d6a2f8d934a2630bebcb
Willem Bouma <wrb@outlook.com>
2015-06-28 10:07:18 +0200

    Add scripts to install Splunk Light 6.2.3

---
+++ Splunk Light/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'SplunkLight'
+$registryUninstallerKeyName = 'SplunkLight'
+$installerType = 'msi'
+$url = 'https://download.splunk.com/products/splunk_light/releases/6.2.3/windows/splunklight-6.2.3-264376-x86-release.msi'
+$url64 = 'https://download.splunk.com/products/splunk_light/releases/6.2.3/windows/splunklight-6.2.3-264376-x64-release.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" ["$url64"  -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"

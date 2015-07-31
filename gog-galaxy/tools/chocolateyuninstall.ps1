938af6ed1c08b535eb3db2a7847857d6da4c1506
Willem Bouma <wrb@outlook.com>
2015-07-31 10:11:59 +0200

    Add GOG Galaxy uninstall script and update nupkg.

---
+++ gog-galaxy/tools/chocolateyuninstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'gog-galaxy'
+$registryUninstallerKeyName = '{7258BA11-600C-430E-A759-27E2C691A335}_is1'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0)
+
+$file = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
+	
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file

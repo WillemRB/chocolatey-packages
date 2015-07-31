79afebd931e054caeb8336b65e2a4c940d0ea4c5
Willem Bouma <wrb@outlook.com>
2015-07-31 11:38:18 +0200

    Update GOG Galaxy package to version 1.0.6.31.

--- gog-galaxy/tools/chocolateyinstall.ps1
+++ gog-galaxy/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'gog-galaxy'
 $registryUninstallerKeyName = '{7258BA11-600C-430E-A759-27E2C691A335}_is1'
 $installerType = 'exe'
-$url = 'http://cdn.gog.com/open/galaxy/client/setup_galaxy_1.0.5.14.exe'
+$url = 'http://cdn.gog.com/open/galaxy/client/setup_galaxy_1.0.6.31.0.exe'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"

6b967de784d73f9adf020e4a4e2f43a91c2f507e
Willem Bouma <wrb@outlook.com>
2015-08-02 19:39:00 +0200

    Add Uplay package

---
+++ uplay/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'uplay'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://ubistatic3-a.akamaihd.net/orbit/launcher_installer/UplayInstaller.exe'
+$url64 = $url
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url
+
+  silentArgs    ='' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT -s"
+  validExitCodes= @(0)
+
+  # optional
+  registryUninstallerKey = '' #ensure this is the value in the registry
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs

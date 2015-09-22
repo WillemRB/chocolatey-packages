f864f12510881b03ec214f3e9895bda9297e8869
Willem Bouma <wrb@outlook.com>
2015-09-22 15:30:38 +0200

    Add HEX Client

---
+++ hextcg/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'hextcg'
+$registryUninstallerKeyName = 'hextcg'
+$installerType = 'exe'
+$url = 'http://dl.hex.gameforge.com/HEXSetup.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
